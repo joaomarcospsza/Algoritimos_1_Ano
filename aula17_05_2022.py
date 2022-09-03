859a45da6653487ec75b5bea5760ed53ef2a803b
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-09-03 15:57:51 -0300

    Aulas
    Aulas de Algoritmos e estrutura de dados.

---
+++ aula17_05_2022.py
@@ -0,0 +1,115 @@
+
+"""n1 = int(input("Informe o primeiro valor para a comparação:"))
+n2 = int(input("Informe o segundo valor para a comparação:"))
+
+if(n1 > n2):
+    print("O maior é n1:", n1)
+else:
+    print("O maior é n2:", n2)
+print("FIM")
+"""
+
+"""Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo."""
+"""valor = int(input("Informe um valor: "))
+if (valor >= 0):
+    print("O numero informado é positivo")
+else:
+    print("O numero informado é negativo")
+"""
+
+"""Faça um programa que peça dois números e imprima na tela somente o maior deles"""
+""""n1 = int(input("Informe o valor do primeiro Numero: "))
+n2 = int(input("Informe o valor do segundo Numero: "))
+
+if (n1 > n2):
+    print("O valor n1:", n1, ", é maior que valor n2: ",n2)
+else:
+    print("O valor n2 é", n2, "é maior que n1: ",n1)
+"""
+"""Faça um programa que peça a idade do usuário. Se ele for maior de idade, apresente a mensagem “Entrada permitida”. Se não, apresente “Entrada negada”."""
+"""idade = int(input("Informe a sua idade: "))
+if (idade >= 18):
+    print("Entrada Permitida")
+else:
+    print("Entrada Negada")"""
+
+"""nota_prova = int(input("Informe a nota da prova: "))
+nota_trabalho = int(input("Informe a nota do tabalho: "))
+
+media = (nota_prova + nota_trabalho)/2
+if( media >= 60):
+     print("Aprovado")
+else:
+    print("Reprovado")
+"""
+
+"""conceito = input("Informe o conceito tirado na disciplina com 'A, B, C, D': ")
+if(conceito == "A"):
+    print("Aprovado")
+
+if(conceito == "B"):
+    print("Aprovado")
+
+if(conceito == "C"):
+    print("Aprovado")
+
+if(conceito == "D"):
+    print("Reprovado")
+"""
+
+"""salario = float(input("Informe o seu salario: "))
+salario_minimo = 1.212
+
+
+if (salario < salario_minimo):
+    print("Você recebe menos que um salario minimo")
+else:
+    qtde = salario / salario_minimo
+    print("Você ganha ", qtde, "de salarios minimos")"""
+
+
+"""sexo = input("Informe o seu sexo com 'F' ou 'M': ").upper()
+if(sexo == "F"):
+    print("Você é do sexo Feminino")
+else:
+    print("Você é do Sexo Masculino")
+"""
+
+"""
+
+idade = int(input("Informe a sua idade: "))
+if (idade >= 18):
+    nome = input("Informe o seu nome: ")
+    print("Bem vindo, ", nome)
+else:
+    print("Entrada não permitida")"""
+"""
+
+idade = int("Informe a sua idade: ")
+if (idade >= 18):
+    salario = float(input("Informe o seu salario: "))
+    aumento = salario * 1.05
+    print("Seu salario agora é de: ",aumento)
+else:
+    print("Calculo não permitido")"""
+
+
+"""numero = int(input("Informe um numero inteiro"))
+if (numero > 0):
+    if( numero < 100):
+        print("Numero no intervalo definido")
+else:
+    print("Numero fora do intervalo")"""
+
+
+"""n1 = int(input("Informe um numero: "))
+if (n1 % 2 == 0):
+    print("o numero", n1, "é", "par")
+else:
+    print("o numero", n1,"é", "impar")"""
+
+"""n1 = int(input("Informe um numero: "))
+if (n1 % 3 == 0):
+    print("o numero", n1, "é", "multiplo")
+else:
+    print("não é multiplo")"""
