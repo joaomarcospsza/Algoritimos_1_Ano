e3e9d231fadd57e2a6a6f13f0b257b5657a1a038
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-09-06 13:34:22 -0300

    Exercícios
    Exercícios da lista de algoritmos vetor 08.1 Introdução

--- exercicios_vetor_sem_laco.py
+++ exercicios_vetor_sem_laco.py
@@ -1,30 +1,25 @@
 """
-Para cada conjunto de valores abaixo, escreva o código necessário para criar vetores:
-0  4  9  18  27  41  65  88  121
-Rafael   Ayslan   Daniela   Frank
-3290,90   128,50   85,90   789,31
-Considerando o vetor criado no exercício 1.a):
-Apresente na tela todos os valores do vetor.
-Apresente na tela todos os valores do vetor em ordem invertida (de trás para frente).
-Crie um vetor com 5 números inteiros de modo que o usuário do programa vá digitar esses valores.
-Apresente na tela todos os valores do vetor.
-Apresente na tela o dobro de todos os valores do vetor.
-Apresente na tela a metade de todos os valores do vetor.
-Crie um vetor de strings para armazenar quatro dados do usuário: nome completo, endereço, telefone e email.
-Apresente na tela, de maneira organizada, todos os dados do usuário, exemplo:
-Nome completo: 	Rafael Zottesso
-Endereço: 		Rua ABC, 123
-Telefone: 		(99) 99999-9999
-E-mail: 			rafael.zottesso@ifpr.edu.br
-Obs.: para dar esse “espaço” utilize o código da tabulação \t. Cada linha precisa de uma quantidade diferente de tabulação. Você pode colocar várias usando \t\t\t, por exemplo.
-Crie um vetor com 4 números decimais (ponto-flutuante) de modo que o usuário do programa vá digitar esses valores.
-Apresente na tela a soma de todos os valores.
-Apresente na tela a média de todos os valores.
-Apresente na tela o produto (multiplicação) de todos os valores.
-Apresente na tela somente quantos valores são maiores do que 1000.
-Apresente na tela a quantidade de valores que são maiores do que zero.
-Apresente na tela somente o maior valor do vetor.
-Apresenta na tela somente o índice do maior valor do vetor.
-Apresente na tela somente o menor valor do vetor.
-Apresenta na tela somente o índice do menor valor do vetor.
+1. Para cada conjunto de valores abaixo, escreva o código necessário para criar vetores:"""
+# a) 0  4  9  18  27  41  65  88  121
+vetor1 = [0,4,9,18,27,41,65,88,121]
+# b) Rafael   Ayslan   Daniela   Frank
+vetor2 = ['Rafael', 'Ayslan', 'Daniela','Frank']
+# c) 3290, 90   128, 50   85, 90   789, 31
+vetor3 = [3290.90, 218.50, 85.90, 789.31]
+vetor = vetor1 + vetor2 + vetor3
+
 """
+2. Considerando o vetor criado no exercício 1.a):"""
+# b) Apresente na tela todos os valores do vetor.
+print(vetor)
+# c) Apresente na tela todos os valores do vetor em ordem invertida (de trás para frente).
+vetor.reverse()
+print(vetor)
+
+"3. Crie um vetor com 5 números inteiros de modo que o usuário do programa vá digitar esses valores."
+
+# a) Apresente na tela todos os valores do vetor.
+
+# b) Apresente na tela o dobro de todos os valores do vetor.
+
+# c) Apresente na tela a metade de todos os valores do vetor.
