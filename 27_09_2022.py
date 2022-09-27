7f100fd366e0f7f4fcc64d2779d08b79cfb42ce2
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-09-27 20:56:52 -0300

    Update 27_09_2022.py
    Definição do que e procedimento e Função

--- 27_09_2022.py
+++ 27_09_2022.py
@@ -5,23 +5,25 @@ def fatorial(num):
     while(i <= num):
         fat = fat * i
         i+= 1
-    print("O valor de {} e o resultado é: {} ".format(num, fat))
+    return fat #expecifica o valor do subalgoritmo
 
 x = int(input("Informe um número: "))
 
-fatorial(x)
+x1 = fatorial(x)
 
 """def fatorial2(fat):
     resultado = 1
     for i in range(fat, 0, -1):
         resultado = resultado * i
     print("Fatorial: {}".format(resultado))
-
-
 fatorial2()"""
 
 """
 Variavel Global: todo o codigo ve
-
 Variavel Local: apenas vista dentro do subalgoritimo
+
+Procedimento: não tem 'return', pq não tem valor
+Função: Tem 'return', pq tem valor
+
+
 """
