84699b193addf32006935febc8c4d574891e335e
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-10-04 19:02:18 -0300

    Update lista_exercicios_funcoes_principal.py

--- lista_exercicios_funcoes_principal.py
+++ lista_exercicios_funcoes_principal.py
@@ -1,4 +1,10 @@
+from Exercicios.lista_exercicios_funcoes import pedirNumero
 import lista_exercicios_funcoes
+
+class pedirNumeroINT:
+    pedirnum = int(input("Informe um número inteiro: "))
+    pd = pedirNumero(pedirnum)
+    print("O número informado foi: {}".format(pd))
 
 pc = lista_exercicios_funcoes.pedirNumero()
 
