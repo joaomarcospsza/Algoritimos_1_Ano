8b9490a956a86b808fd57d4889a9e9e50cfda6fb
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-10-19 16:14:22 -0300

    Questão Número 03
    Fazendo a questão número 03 da lista de funções

--- Listas Exercicios/lista_exercicios_funcoes.py
+++ Listas Exercicios/lista_exercicios_funcoes.py
@@ -1,6 +1,5 @@
 import random
 #1 - Crie uma função para pedir um número inteiro ao usuário e retornar ele. Toda vez que você precisar de um número informado pelo usuário, utilize ela. Ela não tem parâmetro e o retorno é o valor digitado pelo usuário já com o tipo inteiro.
-
 def NumeroInteiro():
     numInt = int(input("Informe um número inteiro: "))
     
@@ -16,3 +15,32 @@ def NumeroAleatorio(n):
     numRand = random.randint(1, n)
     return numRand
 
+#3 - Faça uma função que receba um número inteiro por parâmetro e retorne o mês correspondente ao número. Por exemplo, 2 corresponde a "Fevereiro". Se o número informado não corresponder a um mês (1 a 12), retorne a mensagem “Mês inválido”.
+def RetornaMes(r):
+
+    if(r == 1):
+        print("Mês de Janeiro")
+    elif(r == 2):
+        print("Mês de Fevereiro")
+    elif(r == 3):
+        print("Mês de Março")
+    elif(r == 4):
+        print("Mês de Abril")
+    elif(r == 5):
+        print("Mês de Maio")
+    elif(r == 6):
+        print("Mês de Junho")
+    elif(r == 7):
+        print("Mês de Julho")
+    elif(r == 8):
+        print("Mês de Agosto")
+    elif(r == 9):
+        print("Mês de Setembro")
+    elif(r == 10):
+        print("Mês de Outubro")
+    elif(r == 11):
+        print("Mês de Novembro")
+    elif(r == 12):
+        print("Mês de Dezembro")
+    else:
+        print("Mês invalido")
