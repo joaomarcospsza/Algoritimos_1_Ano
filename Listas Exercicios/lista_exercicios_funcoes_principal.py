6a9fa72f6477d76cfc7817c6e4a7a93c8834659f
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-10-19 16:15:12 -0300

    Questão 03
    Fazendo a questão número 03 da lista de funções

--- Listas Exercicios/lista_exercicios_funcoes_principal.py
+++ Listas Exercicios/lista_exercicios_funcoes_principal.py
@@ -1,8 +1,13 @@
 import lista_exercicios_funcoes
 
+#1
 num = lista_exercicios_funcoes.NumeroInteiro()
-print("Número inteiro informado: ",num)
+print("Número inteiro informado: {}".format(num))
 
+#2
 numAle = lista_exercicios_funcoes.NumeroAleatorio(num)
-print("Número Aléatorio gerado: ", numAle)
+print("Número Aléatorio gerado: {}".format(numAle))
 
+#3
+numMes = lista_exercicios_funcoes.RetornaMes(numAle)
+print("Mês Correspondente: {}".format(numMes))
