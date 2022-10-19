44aa9040bfdd27abf2feca052783c3699ef1d7a4
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-10-19 17:07:05 -0300

    Questões 04 e 05
    Fazendo as atividades 4 e 5 da lista de funções

--- Listas Exercicios/lista_exercicios_funcoes.py
+++ Listas Exercicios/lista_exercicios_funcoes.py
@@ -2,7 +2,6 @@ import random
 #1 - Crie uma função para pedir um número inteiro ao usuário e retornar ele. Toda vez que você precisar de um número informado pelo usuário, utilize ela. Ela não tem parâmetro e o retorno é o valor digitado pelo usuário já com o tipo inteiro.
 def NumeroInteiro():
     numInt = int(input("Informe um número inteiro: "))
-    
     while(numInt < 0):
         print("Número informado {} não e valido;".format(numInt))
         numInt = int(input("Informe um novo número, dessa vez um inteiro: "))
@@ -11,7 +10,6 @@ def NumeroInteiro():
 
 #2 - Faça uma função para gerar números aleatórios. Esta função deve receber um número inteiro num e retornar um número aleatório entre 1 e num. Assim, toda vez que você precisar de um número qualquer pode usar a função do exercício 1 ou esta.
 def NumeroAleatorio(n):
-    
     numRand = random.randint(1, n)
     return numRand
 
@@ -19,28 +17,54 @@ def NumeroAleatorio(n):
 def RetornaMes(r):
 
     if(r == 1):
-        print("Mês de Janeiro")
+        return ("Mês de Janeiro")
     elif(r == 2):
-        print("Mês de Fevereiro")
+        return ("Mês de Fevereiro")
     elif(r == 3):
-        print("Mês de Março")
+        return ("Mês de Março")
     elif(r == 4):
-        print("Mês de Abril")
+        return ("Mês de Abril")
     elif(r == 5):
-        print("Mês de Maio")
+       return ("Mês de Maio")
     elif(r == 6):
-        print("Mês de Junho")
+        return ("Mês de Junho")
     elif(r == 7):
-        print("Mês de Julho")
+        return ("Mês de Julho")
     elif(r == 8):
-        print("Mês de Agosto")
+        return ("Mês de Agosto")
     elif(r == 9):
-        print("Mês de Setembro")
+        return ("Mês de Setembro")
     elif(r == 10):
-        print("Mês de Outubro")
+       return ("Mês de Outubro")
     elif(r == 11):
-        print("Mês de Novembro")
+        return ("Mês de Novembro")
     elif(r == 12):
-        print("Mês de Dezembro")
+        return ("Mês de Dezembro")
     else:
-        print("Mês invalido")
+        return ("Mês invalido")
+
+#4 - Faça funções para resolver as equações de área: do quadrado = x²  do retângulo = b . h 	(base x altura) do triângulo= (b . h)2 do trapézio = (B + b) . h2
+def quadrado(x):
+    qua = x * x
+    return qua
+
+def retangulo(b, a):
+    ret = b * a
+    return ret
+
+def tringulo(b, a):
+    tri = (b * a) /2
+    return tri
+
+def trapezio(B, b, h):
+    tra = ((B + b) * h) /2
+    return tra
+
+#5 - Faça um programa para calcular o Fatorial de um número. Para o cálculo do fatorial, sabemos que n! depende de (n-1)!; este por sua vez depende de (n-2)!; e, assim por diante, até que n seja 1, quando então tem-se que fatorial de 1 é igual a 1 mesmo. Utilize uma função que recebe como parâmetro de entrada o número a ser calculado o fatorial, do tipo inteiro, e retorna o fatorial deste número, também do tipo inteiro
+def fatorial(x):
+    cont = 2
+    fat = 1
+    while(cont <= x):
+        fat *= cont
+        cont+=1
+    return fat
