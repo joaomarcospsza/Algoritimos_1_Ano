496635a3cc6029d632c6235da19be6a87c34cbb0
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-10-26 18:01:58 -0300

    exercício 02
    Exercício 02 da apostila 2

--- Listas Exercicios/lista_exercicios_Heterogênea.py
+++ Listas Exercicios/lista_exercicios_Heterogênea.py
@@ -1,6 +1,8 @@
 
 #Exercício 01: Um telefone celular precisa ser representado em um programa de computador. Sabe-se que este telefone possui uma marca, um modelo, um ano de fabricação e um preço. Implemente uma estrutura para representar os telefones celulares. Crie três celulares, solicite os dados ao usuário para cada um e apresenteos na tela.
+from mimetypes import init
 import time
+from tokenize import Double
 
 #Criando a classe com seus campos e atributos
 class telefone:
@@ -26,3 +28,30 @@ for i in range(0, len(listaTelefone)):
 #Listando todos os itens do vetor
 for i in range(0, len (listaTelefone)):
     print("Marca: {}, Modelo: {}, Ano Fabricação: {}, R$: {} \n".format(listaTelefone[i].marca, listaTelefone[i].modelo, listaTelefone[i].ano_fabricacao, listaTelefone[i].preco,))
+
+
+#Exercício 02: Um programa de computador deve ser capaz de armazenar dados sobre as partes de uma casa.Como a casa possui vários cômodos e cada cômodo possui uma porta, precisa-se de uma estrutura capaz de representar as portas da casa. Sabe-se que toda porta deve ter registrado a sua largura, sua altura, seu peso e 17 o seu material (ex: madeira, alumínio, etc.). Crie três objetos que representem portas (ex: porta da sala, porta do quarto e porta da cozinha). Preencha os dados da porta solicitando-os ao usuário. Em seguida, apresente os dados das portas.
+class porta:
+    def __init__(self):
+        self.porta_comodo = ""
+        self.largura = 0
+        self.altura = 0
+        self.peso = 0
+        self.material = ""
+
+vetor_Porta = [porta()] * 3
+
+for i in range(0, len(porta)):
+    vetor_Porta[i] = porta()
+    vetor_Porta[i].porta_comodo = input("Para qual comodo será essa porta: ")
+    vetor_Porta[i].largura = float(input("Informe a largura da porta: "))
+    vetor_Porta[i].altura = float(input("Informe a altura da porta: "))
+    vetor_Porta[i].peso = float(input("Informe o peso da porta: "))
+    vetor_Porta[i].material = input("Informe o material da porta: ")
+    time.sleep(0.10)
+    print(" ")
+
+for i in range(0, len(vetor_Porta)):
+    print(f"Comodo: {vetor_Porta[i].porta_comodo}, Largura: {vetor_Porta[i].largura}, Altura: {vetor_Porta[i].altura}, Peso: {vetor_Porta[i].peso}, Material: { vetor_Porta[i].material} \n")
+
+
